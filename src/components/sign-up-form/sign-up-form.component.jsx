a9e1f8f2859f1603101b1947e8a29caf20bf6020
bobby iceman <bobby.1c3man@gmail.com>
2022-06-09 15:50:22 +0300

    Rewrote the application from scss to styled components

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -8,7 +8,7 @@ import {
   createUserDocumentFromAuth,
 } from '../../utils/firebase/firebase.utils';
 
-import './sign-up-form.styles.scss';
+import { SignUpContainer } from './sign-up-form.styles';
 
 const defaultFormFields = {
   displayName: '',
@@ -29,19 +29,20 @@ const SignUpForm = () => {
     event.preventDefault();
 
     if (password !== confirmPassword) {
-      alert('Passwords do not match');
+      alert('passwords do not match');
       return;
     }
 
     try {
       const { user } = await createAuthUserWithEmailAndPassword(email, password);
+
       await createUserDocumentFromAuth(user, { displayName });
       resetFormFields();
     } catch (error) {
       if (error.code === 'auth/email-already-in-use') {
         alert('Cannot create user, email already in use');
       } else {
-        console.log('User creation encountered an error', error);
+        console.log('user creation encountered an error', error);
       }
     }
   };
@@ -53,7 +54,7 @@ const SignUpForm = () => {
   };
 
   return (
-    <div className="sign-up-container">
+    <SignUpContainer>
       <h2>Don't have an account?</h2>
       <span>Sign up with your email and password</span>
       <form onSubmit={handleSubmit}>
@@ -85,7 +86,7 @@ const SignUpForm = () => {
         />
 
         <FormInput
-          label="Confirm password"
+          label="Confirm Password"
           type="password"
           required
           onChange={handleChange}
@@ -94,7 +95,7 @@ const SignUpForm = () => {
         />
         <Button type="submit">Sign Up</Button>
       </form>
-    </div>
+    </SignUpContainer>
   );
 };
 
